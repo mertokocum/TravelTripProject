eeea30e4f091592bdf6b9fa5732e29a1b058b630
mertokocum <hmrtkocum@gmail.com>
2024-04-01 00:30:19 +0300

    codefirst classes

---
+++ TravelTripProject/Models/Classes/Comments.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProject.Models.Classes
+{
+    public class Comments
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Mail { get; set; }
+        public string Comment { get; set; }
+        public int BLOGID { get; set; }
+    }
+}
