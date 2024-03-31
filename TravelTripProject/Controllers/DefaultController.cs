523f56e72497a8f565326b84697edaaeaa1a8a4a
mertokocum <hmrtkocum@gmail.com>
2024-03-31 15:18:07 +0300

    Update

--- TravelTripProject/Controllers/DefaultController.cs
+++ TravelTripProject/Controllers/DefaultController.cs
@@ -13,5 +13,10 @@ namespace TravelTripProject.Controllers
         {
             return View();
         }
+
+        public ActionResult About()
+        {
+            return View();
+        }
     }
 }
