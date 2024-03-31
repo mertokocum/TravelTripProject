926477413329a853901e706c2206ef001b411999
mertokocum <hmrtkocum@gmail.com>
2024-04-01 02:34:08 +0300

    About Page Data Listing

---
+++ TravelTripProject/Controllers/AboutController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProject.Controllers;
+using TravelTripProject.Models.Classes;
+
+namespace TravelTripProject.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+
+        private Context c = new Context();
+        public ActionResult Index()
+        {
+            var values = c.AboutMes.ToList();
+            return View(values);
+        }
+    }
+}
