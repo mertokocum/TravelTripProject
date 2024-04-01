f9e53276ca6d5844252ff240d1f4877e77b703c1
mertokocum <hmrtkocum@gmail.com>
2024-04-01 17:18:07 +0300

    Listing Multiple Tables in a View with IEnumerable

--- TravelTripProject/Controllers/BlogController.cs
+++ TravelTripProject/Controllers/BlogController.cs
@@ -18,11 +18,14 @@ namespace TravelTripProject.Controllers
             var vBlogs = c.Blogs.ToList();
             return View(vBlogs);
         }
-
+        BlogComment bc=new BlogComment();
         public ActionResult BlogDetail(int id) //burada  blogdetail/""xx"" olmasını sağladık
          {
-            var findBlog = c.Blogs.Where(x => x.ID == id).ToList(); //blogdetail/x ile dbdeki Blog ID  mizi eşledik
-            return View(findBlog);
+
+            //var findBlog = c.Blogs.Where(x => x.ID == id).ToList(); //blogdetail/x ile dbdeki Blog ID  mizi eşledik
+            bc.Value1 = c.Blogs.Where(x => x.ID == id).ToList();
+
+            return View(bc);
 
         }
     }
