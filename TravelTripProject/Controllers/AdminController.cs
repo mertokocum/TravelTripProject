5331923cfcc984fbdcf34f89f06d0fdf4352b174
mertokocum <hmrtkocum@gmail.com>
2024-04-08 00:44:11 +0300

    Blog List >delete update

--- TravelTripProject/Controllers/AdminController.cs
+++ TravelTripProject/Controllers/AdminController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProject.Models.Classes;
 
 namespace TravelTripProject.Controllers
 {
     public class AdminController : Controller
     {
         // GET: Admin
+        private Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var values = c.Blogs.ToList();
+            return View(values);
         }
     }
 }
