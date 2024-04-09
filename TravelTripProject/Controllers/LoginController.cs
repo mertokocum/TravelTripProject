8635287e6b6462d3318d59da79a7cde405b9bd0f
mertokocum <hmrtkocum@gmail.com>
2024-04-09 21:53:06 +0300

    Login

--- TravelTripProject/Controllers/LoginController.cs
+++ TravelTripProject/Controllers/LoginController.cs
@@ -3,16 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using TravelTripProject.Models.Classes;
 
 namespace TravelTripProject.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
-        
+        private Context c = new Context();
         public ActionResult Login()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Admin ad)
+        {
+            var info = c.Admins.FirstOrDefault(x => x.AdminUsername == ad.AdminUsername && x.Password == ad.Password);
+            if (info != null)
+            {
+                FormsAuthentication.SetAuthCookie(info.AdminUsername,false);
+                Session["AdminUsername"]=info.AdminUsername.ToString();
+                return RedirectToAction("Index", "Admin");
+            }
+            else
+            {
+                return View();
+            }
+            
         }
     }
 }
